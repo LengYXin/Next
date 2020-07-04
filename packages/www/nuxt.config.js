432cfa8e8e3d4c63365f6cb9648fa91a2b7bad3c
LengYXin <lengyingxin8966@gmail.com>
2020-07-04 14:19:45 +0800

    update

--- packages/www/nuxt.config.js
+++ packages/www/nuxt.config.js
@@ -1,27 +1,33 @@
-
-const webpack = require('webpack');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 const lodash = require('lodash');
 const path = require('path');
-module.exports = {
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
   mode: 'universal',
   /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'static',
+  /*
   ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
     title: '暄桐教室',//process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'version', name: 'version', content: process.env.npm_package_version || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
   /*
   ** Global CSS
   */
@@ -30,10 +36,16 @@ module.exports = {
   ],
   /*
   ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
     '@/plugins/use'
   ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
   /*
   ** Nuxt.js dev-modules
   */
@@ -47,6 +59,7 @@ module.exports = {
   ],
   /*
   ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
     /*
@@ -56,43 +69,37 @@ module.exports = {
       // 图库替换
       lodash.set(config, `resolve.alias["@ant-design/icons/lib/dist$"]`, path.resolve(process.cwd(), 'plugins/icon.ts'))
     },
+    extractCSS: true,
     loaders: {
       less: {
         javascriptEnabled: true
       }
     },
-    plugins: [
-      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    ],
-    babel: {
-      plugins: [
-        // [
-        //   "import",
-        //   { libraryName: "ant-design-vue", libraryDirectory: "es", style: true }
-        // ]
-      ]
-    },
-    extractCSS: true,
-    optimization: {
-      splitChunks: {
-        chunks: 'async',
-        // cacheGroups: {
-        //   commons: {
-        //     test: /[\\/]node_modules[\\/](vue|ant-.*|@ant-.*|vue-.*|babel-.*|core-js|moment|lodash)[\\/]/,
-        //     name: 'vendors',
-        //     chunks: 'all'
-        //   }
-        // }
-        //   pages: {
-        //     test: /[\\/]pages[\\/]/,
-        //     name: 'pages',
-        //     chunks: 'all'
-        //   }
-        // }
-      }
-    },
     analyze: {
       analyzerMode: 'static'
+    },
+    plugins: [
+      new MomentLocalesPlugin({ localesToKeep: ['es-us', 'zh-cn'] })
+    ],
+    splitChunks: {
+      layouts: false,
+      pages: true,
+      commons: false
+    },
+    // transpile: ['ant-design-vue'],
+    babel: {
+      plugins: [
+        // ['import', {
+        //   libraryName: 'ant-design-vue',
+        //   libraryDirectory: 'es',
+        //   style: true
+        // }, 'ant-design-vue'],
+        // ['import', {
+        //   libraryName: 'lodash',
+        //   libraryDirectory: '',
+        //   camel2DashComponentName: false
+        // }, 'lodash']
+      ]
     }
   }
 }
