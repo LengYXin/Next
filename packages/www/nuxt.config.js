22f9aee0dd1851493d62979cc5331787a3949a71
LengYXin <lengyingxin8966@gmail.com>
2020-07-17 19:15:16 +0800

    update

--- packages/www/nuxt.config.js
+++ packages/www/nuxt.config.js
@@ -1,6 +1,5 @@
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 const lodash = require('lodash');
-const path = require('path');
+const build = require('./configs/build');
 module.exports = {
   /*
   ** Nuxt rendering mode
@@ -68,52 +67,5 @@ module.exports = {
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
   */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      // 图库替换
-      lodash.set(config, `resolve.alias["@ant-design/icons/lib/dist$"]`, path.resolve(process.cwd(), 'plugins/icon.ts'));
-      // lodash.unset(config, 'resolve.alias.@');
-      // console.log("extend -> config", config)
-    },
-    extractCSS: true,
-    loaders: {
-      less: {
-        javascriptEnabled: true
-      }
-    },
-    analyze: {
-      analyzerMode: 'static'
-    },
-    plugins: [
-      new MomentLocalesPlugin({ localesToKeep: ['es-us', 'zh-cn'] })
-    ],
-    optimization: {
-      splitChunks: {
-        chunks: 'async',
-      }
-    },
-    splitChunks: {
-      layouts: false,
-      pages: true,
-      commons: false
-    },
-    // transpile: ['ant-design-vue'],
-    babel: {
-      plugins: [
-        // ['import', {
-        //   libraryName: 'ant-design-vue',
-        //   libraryDirectory: 'es',
-        //   style: true
-        // }, 'ant-design-vue'],
-        // ['import', {
-        //   libraryName: 'lodash',
-        //   libraryDirectory: '',
-        //   camel2DashComponentName: false
-        // }, 'lodash']
-      ]
-    }
-  }
+  build: build
 }
