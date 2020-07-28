60a288b94704213bc970464f108b7e97bccba496
LengYXin <lengyingxin8966@gmail.com>
2020-07-28 20:19:38 +0800

    update

--- packages/main/src/register.tsx
+++ packages/main/src/register.tsx
@@ -2,42 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { registerMicroApps, runAfterFirstMounted, setDefaultMountApp, start, initGlobalState } from 'qiankun';
 export default (props: any, container: HTMLDivElement, onLoading: (loader: Boolean) => void) => {
-    registerMicroApps(
-        [
+    try {
+        registerMicroApps(
+            [
+                {
+                    name: 'vue',
+                    entry: '//localhost:8080',
+                    activeRule: '/vue',
+                    container: container,
+                    props: props,
+                    loader: onLoading,
+                },
+                {
+                    name: 'react',
+                    entry: '//localhost:3001',
+                    activeRule: '/test',
+                    container: container,
+                    props: props,
+                    loader: onLoading,
+                },
+            ],
             {
-                name: 'vue',
-                entry: '//localhost:8080',
-                activeRule: '/vue',
-                container: container,
-                props: props,
-                loader: onLoading,
+                //   beforeLoad: [
+                //     app => {
+                //       console.log('[LifeCycle] before load %c%s', 'color: green;', app.name);
+                //     },
+                //   ],
+                //   beforeMount: [
+                //     app => {
+                //       console.log('[LifeCycle] before mount %c%s', 'color: green;', app.name);
+                //     },
+                //   ],
+                //   afterUnmount: [
+                //     app => {
+                //       console.log('[LifeCycle] after unmount %c%s', 'color: green;', app.name);
+                //     },
+                //   ],
             },
-            {
-                name: 'react',
-                entry: '//localhost:3002',
-                activeRule: '/test',
-                container: container,
-                props: props,
-                loader: onLoading,
-            },
-        ],
-        {
-            //   beforeLoad: [
-            //     app => {
-            //       console.log('[LifeCycle] before load %c%s', 'color: green;', app.name);
-            //     },
-            //   ],
-            //   beforeMount: [
-            //     app => {
-            //       console.log('[LifeCycle] before mount %c%s', 'color: green;', app.name);
-            //     },
-            //   ],
-            //   afterUnmount: [
-            //     app => {
-            //       console.log('[LifeCycle] after unmount %c%s', 'color: green;', app.name);
-            //     },
-            //   ],
-        },
-    );
-    start({ prefetch: false });
+        );
+        start({ prefetch: false });
+    } catch (error) {
+        console.log("LENG: error", error)
+    }
 }
