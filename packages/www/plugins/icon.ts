d007cfcab3b25db6014169a17e7bd1e7ce301850
欧尼酱 <568920429@qq.com>
2020-08-20 22:56:13 +0800

    update:首页写字课样式

--- packages/www/plugins/icon.ts
+++ packages/www/plugins/icon.ts
@@ -4,133 +4,65 @@
  * @create date 2020-07-03 16:30:08
  * @modify date 2020-07-03 16:30:08
  * @desc [description]
- * 重置 antd icon 
+ * 重置 antd icon
  */
 
-export {
-    default as QuestionCircleOutline
-} from '@ant-design/icons/lib/outline/QuestionCircleOutline';
+export { default as QuestionCircleOutline } from "@ant-design/icons/lib/outline/QuestionCircleOutline";
 
-export {
-    default as QuestionOutline
-} from '@ant-design/icons/lib/outline/QuestionOutline';
+export { default as QuestionOutline } from "@ant-design/icons/lib/outline/QuestionOutline";
 
-export {
-    default as LoadingOutline
-} from '@ant-design/icons/lib/outline/LoadingOutline';
+export { default as LoadingOutline } from "@ant-design/icons/lib/outline/LoadingOutline";
 
-export {
-    default as CloseOutline
-} from '@ant-design/icons/lib/outline/CloseOutline';
-export {
-    default as CloseCircleOutline
-} from '@ant-design/icons/lib/outline/CloseCircleOutline';
-export {
-    default as CloseCircleFill
-} from '@ant-design/icons/lib/fill/CloseCircleFill';
-export {
-    default as CloseSquareFill
-} from '@ant-design/icons/lib/fill/CloseSquareFill';
-export {
-    default as InfoCircleOutline
-} from '@ant-design/icons/lib/outline/InfoCircleOutline';
-export {
-    default as InfoCircleFill
-} from '@ant-design/icons/lib/fill/InfoCircleFill';
-export {
-    default as InfoOutline
-} from '@ant-design/icons/lib/outline/InfoOutline';
+export { default as CloseOutline } from "@ant-design/icons/lib/outline/CloseOutline";
+export { default as CloseCircleOutline } from "@ant-design/icons/lib/outline/CloseCircleOutline";
+export { default as CloseCircleFill } from "@ant-design/icons/lib/fill/CloseCircleFill";
+export { default as CloseSquareFill } from "@ant-design/icons/lib/fill/CloseSquareFill";
+export { default as InfoCircleOutline } from "@ant-design/icons/lib/outline/InfoCircleOutline";
+export { default as InfoCircleFill } from "@ant-design/icons/lib/fill/InfoCircleFill";
+export { default as InfoOutline } from "@ant-design/icons/lib/outline/InfoOutline";
 
-export {
-    default as ExclamationCircleOutline
-} from '@ant-design/icons/lib/outline/ExclamationCircleOutline';
-export {
-    default as ExclamationCircleFill
-} from '@ant-design/icons/lib/fill/ExclamationCircleFill';
+export { default as ExclamationCircleOutline } from "@ant-design/icons/lib/outline/ExclamationCircleOutline";
+export { default as ExclamationCircleFill } from "@ant-design/icons/lib/fill/ExclamationCircleFill";
 
-export {
-    default as ExclamationOutline
-} from '@ant-design/icons/lib/outline/ExclamationOutline';
+export { default as ExclamationOutline } from "@ant-design/icons/lib/outline/ExclamationOutline";
 // 勾
-export {
-    default as CheckCircleOutline
-} from '@ant-design/icons/lib/outline/CheckCircleOutline';
+export { default as CheckCircleOutline } from "@ant-design/icons/lib/outline/CheckCircleOutline";
 
-export {
-    default as CheckOutline
-} from '@ant-design/icons/lib/outline/CheckOutline';
-export {
-    default as CheckCircleFill
-} from '@ant-design/icons/lib/fill/CheckCircleFill';
+export { default as CheckOutline } from "@ant-design/icons/lib/outline/CheckOutline";
+export { default as CheckCircleFill } from "@ant-design/icons/lib/fill/CheckCircleFill";
 // 左
-export {
-    default as LeftCircleOutline
-} from '@ant-design/icons/lib/outline/LeftCircleOutline';
+export { default as LeftCircleOutline } from "@ant-design/icons/lib/outline/LeftCircleOutline";
 
-export {
-    default as LeftOutline
-} from '@ant-design/icons/lib/outline/LeftOutline';
+export { default as LeftOutline } from "@ant-design/icons/lib/outline/LeftOutline";
 // 右
-export {
-    default as RightCircleOutline
-} from '@ant-design/icons/lib/outline/RightCircleOutline';
+export { default as RightCircleOutline } from "@ant-design/icons/lib/outline/RightCircleOutline";
 
-export {
-    default as RightOutline
-} from '@ant-design/icons/lib/outline/RightOutline';
+export { default as RightOutline } from "@ant-design/icons/lib/outline/RightOutline";
 // 搜索
-export {
-    default as SearchOutline
-} from '@ant-design/icons/lib/outline/SearchOutline';
+export { default as SearchOutline } from "@ant-design/icons/lib/outline/SearchOutline";
 // 通知
-export {
-    default as BellOutline
-} from '@ant-design/icons/lib/outline/BellOutline';
+export { default as BellOutline } from "@ant-design/icons/lib/outline/BellOutline";
 // 省略号
-export {
-    default as EllipsisOutline
-} from '@ant-design/icons/lib/outline/EllipsisOutline';
+export { default as EllipsisOutline } from "@ant-design/icons/lib/outline/EllipsisOutline";
 // 点赞
-export {
-    default as LikeOutline
-} from '@ant-design/icons/lib/outline/LikeOutline';
-export {
-    default as LikeFill
-} from '@ant-design/icons/lib/fill/LikeFill';
+export { default as LikeOutline } from "@ant-design/icons/lib/outline/LikeOutline";
+export { default as LikeFill } from "@ant-design/icons/lib/fill/LikeFill";
 // 笑脸
-export {
-    default as SmileOutline
-} from '@ant-design/icons/lib/outline/SmileOutline';
+export { default as SmileOutline } from "@ant-design/icons/lib/outline/SmileOutline";
 // 消息
-export {
-    default as MessageOutline
-} from '@ant-design/icons/lib/outline/MessageOutline';
+export { default as MessageOutline } from "@ant-design/icons/lib/outline/MessageOutline";
 // 图片
-export {
-    default as PictureOutline
-} from '@ant-design/icons/lib/outline/PictureOutline';
+export { default as PictureOutline } from "@ant-design/icons/lib/outline/PictureOutline";
 // 爱心
-export {
-    default as HeartOutline
-} from '@ant-design/icons/lib/outline/HeartOutline';
-export {
-    default as HeartFill
-} from '@ant-design/icons/lib/fill/HeartFill';
+export { default as HeartOutline } from "@ant-design/icons/lib/outline/HeartOutline";
+export { default as HeartFill } from "@ant-design/icons/lib/fill/HeartFill";
 // 人
-export {
-    default as TeamOutline
-} from '@ant-design/icons/lib/outline/TeamOutline';
+export { default as TeamOutline } from "@ant-design/icons/lib/outline/TeamOutline";
 // 播放
-export {
-    default as PlayCircleOutline
-} from '@ant-design/icons/lib/outline/PlayCircleOutline';
+export { default as PlayCircleOutline } from "@ant-design/icons/lib/outline/PlayCircleOutline";
 // 删除
-export {
-    default as DeleteOutline
-} from '@ant-design/icons/lib/outline/DeleteOutline';
-export {
-    default as EyeOutline
-} from '@ant-design/icons/lib/outline/EyeOutline';
-export {
-    default as PlusOutline
-} from '@ant-design/icons/lib/outline/PlusOutline';
+export { default as DeleteOutline } from "@ant-design/icons/lib/outline/DeleteOutline";
+export { default as EyeOutline } from "@ant-design/icons/lib/outline/EyeOutline";
+export { default as PlusOutline } from "@ant-design/icons/lib/outline/PlusOutline";
+// 下箭头
+export { default as CaretDownOutline } from "@ant-design/icons/lib/outline/CaretDownOutline";
