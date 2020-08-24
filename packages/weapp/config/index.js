5d641ac3acd8655e6d75483ba8ce139fd5c334d5
LengYXin <lengyingxin8966@gmail.com>
2020-08-24 12:16:55 +0800

    update

--- packages/weapp/config/index.js
+++ packages/weapp/config/index.js
@@ -70,52 +70,52 @@ const config = {
     }
   }
 }
-const updateKey = 'LENG magicChange'
-magicChange()
-magicChangeTransform()
+// const updateKey = 'LENG magicChange'
+// magicChange()
+// magicChangeTransform()
 module.exports = function (merge) {
   if (process.env.NODE_ENV === 'development') {
     return merge({}, config, require('./dev'))
   }
   return merge({}, config, require('./prod'))
 }
-function magicChange() {
-  try {
-    const startPath = require.resolve('@tarojs/router').replace('index.js', 'router.esm.js');
-    // 文件存在
-    if (fs.existsSync(startPath)) {
-      const startjs = fs.readFileSync(startPath).toString();
-      if (!lodash.includes(startjs, updateKey)) {
-        console.log('------------------------------------ 修改 h5 下拉刷新无效 ------------------------------------')
-        // 替换 代码
-        const newStartjs = startjs.replace('onLoad = false) {', `onLoad = true) { //${updateKey} `);
-        fs.writeFileSync(startPath, newStartjs);
-      }
-    }
-  } catch (error) {
-    console.log("modify failed", error);
-  }
-}
-function magicChangeTransform() {
-  try {
-    const startPath = require.resolve('@tarojs/components').replace('index.js', 'esm-es5/taro-pull-to-refresh.entry.js');
-    // 文件存在
-    if (fs.existsSync(startPath)) {
-      const startjs = fs.readFileSync(startPath).toString();
-      if (!lodash.includes(startjs, updateKey)) {
-        console.log('------------------------------------ 修改 translate3d 影响模态框定位 ------------------------------------')
-        // 替换 代码
-        const newStartjs = startjs.replace('setTransform(_this.contentRef.style, "translate3d(0px," + ty + "px,0)");', `
-          if (ty) {
-            setTransform(_this.contentRef.style, "translate3d(0px," + ty + "px,0)"); //${updateKey}
-          } else {
-              _this.contentRef.style = ''
-          }
-        `);
-        fs.writeFileSync(startPath, newStartjs);
-      }
-    }
-  } catch (error) {
-    console.log("modify failed", error);
-  }
-}
+// function magicChange() {
+//   try {
+//     const startPath = require.resolve('@tarojs/router').replace('index.js', 'router.esm.js');
+//     // 文件存在
+//     if (fs.existsSync(startPath)) {
+//       const startjs = fs.readFileSync(startPath).toString();
+//       if (!lodash.includes(startjs, updateKey)) {
+//         console.log('------------------------------------ 修改 h5 下拉刷新无效 ------------------------------------')
+//         // 替换 代码
+//         const newStartjs = startjs.replace('onLoad = false) {', `onLoad = true) { //${updateKey} `);
+//         fs.writeFileSync(startPath, newStartjs);
+//       }
+//     }
+//   } catch (error) {
+//     console.log("modify failed", error);
+//   }
+// }
+// function magicChangeTransform() {
+//   try {
+//     const startPath = require.resolve('@tarojs/components').replace('index.js', 'esm-es5/taro-pull-to-refresh.entry.js');
+//     // 文件存在
+//     if (fs.existsSync(startPath)) {
+//       const startjs = fs.readFileSync(startPath).toString();
+//       if (!lodash.includes(startjs, updateKey)) {
+//         console.log('------------------------------------ 修改 translate3d 影响模态框定位 ------------------------------------')
+//         // 替换 代码
+//         const newStartjs = startjs.replace('setTransform(_this.contentRef.style, "translate3d(0px," + ty + "px,0)");', `
+//           if (ty) {
+//             setTransform(_this.contentRef.style, "translate3d(0px," + ty + "px,0)"); //${updateKey}
+//           } else {
+//               _this.contentRef.style = ''
+//           }
+//         `);
+//         fs.writeFileSync(startPath, newStartjs);
+//       }
+//     }
+//   } catch (error) {
+//     console.log("modify failed", error);
+//   }
+// }
