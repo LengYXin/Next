305fb087b04e2eddbb658c767a605e00d295ee4a
LengYXin <lengyingxin8966@gmail.com>
2020-08-27 19:52:18 +0800

    update

--- packages/www/pages/video/_id/index.vue
+++ packages/www/pages/video/_id/index.vue
@@ -2,8 +2,12 @@
   <div class="xt-content xt-videoid">
     <div>
       <h1 v-text="PageStore.dataSource.title"></h1>
-      <VeView :item="PageStore.dataSource" />
-      <VeLike :item="PageStore.dataSource" />
+      <a-space size="large">
+        <time v-dateFormat="PageStore.dataSource.publishTime" format="YYYY-MM-DD" />
+        <VeView :item="PageStore.dataSource" />
+        <xt-action title="0" icon="message" :statistics="PageStore.dataSource.commentCount" />
+        <VeLike :item="PageStore.dataSource" />
+      </a-space>
     </div>
     <xt-dplayer :options="options" @playing="onPlaying" />
 
@@ -22,12 +26,14 @@
 </template>
 <script lang="ts">
 import { Component, Prop, Vue, Provide, Inject } from "vue-property-decorator";
+import { Observer } from "mobx-vue";
 import { Context } from "@nuxt/types";
 import lodash from "lodash";
 import VeLike from "../views/like.vue";
 import VeView from "../views/view.vue";
 import VeComment from "./views/comment.vue";
 import VeRecommend from "./views/recommend.vue";
+@Observer
 @Component({
   async fetch(ctx: Context) {
     const store = ctx.store.$storeVideo.Details;
@@ -39,6 +45,9 @@ import VeRecommend from "./views/recommend.vue";
   components: { VeLike, VeView, VeComment, VeRecommend },
 })
 export default class PageView extends Vue {
+  get id() {
+    return this.$route.params.id;
+  }
   @Provide("VideoStore")
   get VideoStore() {
     return this.$store.$storeVideo;
@@ -59,7 +68,7 @@ export default class PageView extends Vue {
     console.log("onLike");
   }
   onPlaying() {
-    console.log("aaa");
+    this.PageStore.onPlayNumber(this.id);
   }
   created() {
     this.$setBreadcrumb({ linksName: this.PageStore.dataSource.title }, true);
