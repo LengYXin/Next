d7d87b521711c04c05df0a426758245ae0d2139c
LengYXin <lengyingxin8966@gmail.com>
2020-08-27 14:42:56 +0800

    update

--- packages/www/configs/build.js
+++ packages/www/configs/build.js
@@ -126,11 +126,11 @@ module.exports = {
     babel: {
         plugins: [
             'lodash',
-            // ['import', {
-            //   libraryName: 'ant-design-vue',
-            //   libraryDirectory: 'es',
-            //   style: 'less'
-            // }],
+            ['import', {
+              libraryName: 'ant-design-vue',
+              libraryDirectory: 'lib',
+              style: (name) => `${name}/style/index.js`
+            }],
             // ['import', {
             //   libraryName: 'lodash',
             //   libraryDirectory: '',
